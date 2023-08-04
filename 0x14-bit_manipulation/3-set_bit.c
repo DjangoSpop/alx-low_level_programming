798a17552334d6af7fa7ae1ee013adf41bc8b4b7
DjangoSpop <behodjango@gmail.com>
2023-08-04 03:44:54 +0300

    we can do this

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,39 +1,16 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-<<<<<<< HEAD
- * set_bit - sets the value of a bit to 1.
- * at a given index.
- * @n: pointer of an unsigned long int.
- * @index: index of the bit.
- *
- * Return: 1 if it worked, -1 if it didn't.
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: parameter
+ * @index: index
+ * Return: value
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int m;
-
-	if (index > 63)
-		return (-1);
-
-	m = 1 << index;
-	*n = (*n | m);
-
-	return (1);
-=======
- * set_bit - Sets the value of a bit to 1 at a given index.
- * @n: A pointer to the number.
- * @index: The index of the bit to set.
- *
- * Return: 1 if it worked, or -1 if an error occurred.
- */
-int set_bit(unsigned long int *n, unsigned int index)
-{
-    if (index >= (sizeof(unsigned long int) * 8))
-        return -1;
-
-    unsigned long int mask = 1UL << index;
-    *n = *n | mask;
-    return 1;
->>>>>>> refs/remotes/origin/main
+if (index > sizeof(n) * 8)
+return (-1);
+*n |= (1 << index);
+return (1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,42 +1,22 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * clear_bit - sets the value of a bit to 0.
- * at a given index.
- * @n: pointer of an unsigned long int.
- * @index: index of the bit.
- *
- * Return: 1 if it worked, -1 if it didn't.
+ * flip_bits - number of bits you would need to flip
+ * to get from one number to another
+ * @n: parameter
+ * @m: destiny
+ * Return: nflips
  */
-int clear_bit(unsigned long int *n, unsigned int index)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int m;
-
-	if (index > 63)
-		return (-1);
-
-	m = 1 << index;
-
-	if (*n & m)
-		*n ^= m;
-
-	return (1);
-=======
- * clear_bit - Sets the value of a bit to 0 at a given index.
- * @n: A pointer to the number.
- * @index: The index of the bit to clear.
- *
- * Return: 1 if it worked, or -1 if an error occurred.
- */
-int clear_bit(unsigned long int *n, unsigned int index)
+unsigned int i, nflips = 0;
+unsigned long int j = sizeof(unsigned long int) * 8;
+for (i = 0; i < j; i++)
 {
-    if (index >= (sizeof(unsigned long int) * 8))
-        return -1;
-
-    unsigned long int mask = 1UL << index;
-    mask = ~mask; // Invert the mask to have a 0 at the index and 1s elsewhere
-    *n = *n & mask;
-    return 1;
->>>>>>> refs/remotes/origin/main
+if ((m & 1) != (n & 1))
+nflips += 1;
+n = n >> 1;
+m = m >> 1;
+}
+return (nflips);
 }
